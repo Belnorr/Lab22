4b3e56c286a5c0aa3e828b26681bfba5a40b20de
LeiShweSin <leishwe.24@ichat.sp.edu.sg>
2025-05-22 21:50:50 +0800

    added bmi.py

---
+++ bmi.py
@@ -0,0 +1,27 @@
+#functions
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))        #str() function changes the input value to string
+    print("Weight = " + str(weight))
+
+    bmi = weight/(height**2)
+    print("BMI = " + str(bmi))
+    return bmi
+
+    
+def classify_bmi(bmi):
+    if(bmi<18.5):
+        print("Under Weight")
+    elif(bmi>=18.5 and bmi<=25.0):
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    bmi_calculated = calculate_bmi(weight=57, height=1.73)  #can only concatenate str to str
+    classify_bmi(bmi_calculated)
+    print()
+
+
+if __name__ == "__main__":
+    main()
